cff3b46f0fe72a7f9dd4a9b0e3958f174cd27fd7
JosephAlleemWaldron <josephalleemwaldron@gmail.com>
2024-04-12 06:49:08 -0400

    commit

---
+++ javascript/homepage.js
@@ -0,0 +1,45 @@
+const slides = document.querySelectorAll(".slides p");
+let slideIndex = 0;
+
+
+//initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider );
+
+function initializeSlider(){
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+        
+
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+
+    });
+    slides[slideIndex].classList.add("displaySlide");
+
+
+}
+
+function prevslide(){
+    slideIndex--;
+    showSlide(slideIndex);
+
+}
+
+
+function nextslide(){
+    slideIndex++;
+    showSlide(slideIndex);
+
+
+
+}
